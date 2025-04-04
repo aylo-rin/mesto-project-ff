35e34c631a84a017adcd8bb88c26c13130aff191
Kseniia Mazalova <mazalova.ksyusha21@gmail.com>
2025-04-04 21:32:15 +0300

    script added

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,55 @@
-// @todo: Темплейт карточки
+const container = document.querySelector(".places");
+const cardContainer = container.querySelector(".places__list");
+const addCardButton = document.querySelector(".profile__add-button");
+const newCardPopUp = document.querySelector(".popup_type_new-card");
+const closeCardButton = newCardPopUp.querySelector(".popup__close");
+const saveNewCardButton = newCardPopUp.querySelector(".popup__button");
 
-// @todo: DOM узлы
+addCardButton.addEventListener("click", function () {
+  newCardPopUp.classList.add("popup_is-opened");
+});
 
-// @todo: Функция создания карточки
+closeCardButton.addEventListener("click", function () {
+  newCardPopUp.classList.remove("popup_is-opened");
+});
 
-// @todo: Функция удаления карточки
+function addCard(name, link) {
+  const cardTemplate = document.querySelector("#card-template").content;
+  const cardElement = cardTemplate.querySelector(".card").cloneNode(true);
 
-// @todo: Вывести карточки на страницу
+  cardElement.querySelector(".card__title").textContent = name;
+  cardElement.querySelector(".card__image").src = link;
+
+  cardElement
+    .querySelector(".card__like-button")
+    .addEventListener("click", function (event) {
+      event.target.classList.toggle("card__like-button_is-active");
+    });
+
+  cardElement
+    .querySelector(".card__delete-button")
+    .addEventListener("click", function () {
+      cardElement.remove();
+    });
+
+  cardContainer.append(cardElement);
+}
+
+saveNewCardButton.addEventListener("click", function () {
+  const name = document.querySelector(".popup__input_type_card-name");
+  const link = document.querySelector(".popup__input_type_url");
+  newCardPopUp.classList.remove("popup_is-opened");
+
+  addCard(name.value, link.value);
+
+  name.value = "";
+  link.value = "";
+});
+
+function renderInitialCards() {
+  initialCards.forEach((card) => {
+    addCard(card.name, card.link);
+  });
+}
+
+renderInitialCards();
