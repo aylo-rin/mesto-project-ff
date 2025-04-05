216b1f00506dfae94b8c4d892679c51f1cc6fa60
Kseniia Mazalova <mazalova.ksyusha21@gmail.com>
2025-04-05 22:30:32 +0300

    fixed script errors

--- scripts/index.js
+++ scripts/index.js
@@ -4,6 +4,9 @@ const addCardButton = document.querySelector(".profile__add-button");
 const newCardPopUp = document.querySelector(".popup_type_new-card");
 const closeCardButton = newCardPopUp.querySelector(".popup__close");
 const saveNewCardButton = newCardPopUp.querySelector(".popup__button");
+const cardTemplate = document.querySelector("#card-template").content;
+const cardNameInput = document.querySelector(".popup__input_type_card-name");
+const cardLinkInput = document.querySelector(".popup__input_type_url");
 
 addCardButton.addEventListener("click", function () {
   newCardPopUp.classList.add("popup_is-opened");
@@ -11,39 +14,60 @@ addCardButton.addEventListener("click", function () {
 
 closeCardButton.addEventListener("click", function () {
   newCardPopUp.classList.remove("popup_is-opened");
+
+  cardNameInput.value = "";
+  cardLinkInput.value = "";
 });
 
-function addCard(name, link) {
-  const cardTemplate = document.querySelector("#card-template").content;
-  const cardElement = cardTemplate.querySelector(".card").cloneNode(true);
-
-  cardElement.querySelector(".card__title").textContent = name;
-  cardElement.querySelector(".card__image").src = link;
-
+function likeCard(cardElement) {
   cardElement
     .querySelector(".card__like-button")
     .addEventListener("click", function (event) {
       event.target.classList.toggle("card__like-button_is-active");
     });
+}
 
+function deleteCard(cardElement) {
   cardElement
     .querySelector(".card__delete-button")
     .addEventListener("click", function () {
       cardElement.remove();
     });
-
-  cardContainer.append(cardElement);
 }
 
-saveNewCardButton.addEventListener("click", function () {
-  const name = document.querySelector(".popup__input_type_card-name");
-  const link = document.querySelector(".popup__input_type_url");
+function createCard(item, { deleteCard, likeCard }) {
+  const cardElement = cardTemplate.querySelector(".card").cloneNode(true);
+  const cardImage = cardElement.querySelector(".card__image");
+
+  cardElement.querySelector(".card__title").textContent = item.name;
+  cardImage.src = item.link;
+  cardImage.alt = item.name;
+
+  likeCard(cardElement);
+  deleteCard(cardElement);
+
+  return cardElement;
+}
+
+function addCard(name, link) {
+  const newCard = createCard({ name, link }, { deleteCard, likeCard });
+  cardContainer.append(newCard);
+}
+
+saveNewCardButton.addEventListener("click", function (event) {
+  event.preventDefault();
+
+  if (!cardNameInput.value || !cardLinkInput.value) {
+    alert("Пожалуйста, заполните все поля ввода");
+    return;
+  }
+
+  addCard(cardNameInput.value, cardLinkInput.value);
+
+  cardNameInput.value = "";
+  cardLinkInput.value = "";
+
   newCardPopUp.classList.remove("popup_is-opened");
-
-  addCard(name.value, link.value);
-
-  name.value = "";
-  link.value = "";
 });
 
 function renderInitialCards() {
